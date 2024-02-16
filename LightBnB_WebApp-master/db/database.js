61bb2a37f19a636f451adf1fe3e3f918b5b12bfa
Taha Mulla <tahamulla@hotmail.com>
2024-02-15 19:23:19 -0500

    Refactored getUserWithEmail, getUserWithId, addUserto use sql query and promises

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -18,14 +18,28 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  return new Promise((resolve, reject) => {
+    const queryParams = [email];
+    let queryStr = `
+      SELECT *
+      FROM users
+      WHERE email = $1;
+      `;
+
+    pool
+      .query(queryStr, queryParams)
+      .then(result => {
+        let user = result.rows[0];
+        if (user.email.toLowerCase() === email.toLowerCase()) {
+          resolve(user);
+        } else {
+          user = null;
+          resolve(user);
+        }
+      })
+      .catch(error => reject(error));
+  });
+
 };
 
 /**
@@ -34,7 +48,27 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return new Promise((resolve, reject) => {
+    const queryParams = [id];
+    let queryStr = `
+      SELECT *
+      FROM users
+      WHERE id = $1;
+      `;
+
+    pool
+      .query(queryStr, queryParams)
+      .then((result) => {
+        let user = result.rows[0];
+        if (user.id === id) {
+          resolve(user);
+        } else {
+          user = null;
+          resolve(user);
+        }
+      })
+      .catch(error => reject(error));
+  });
 };
 
 /**
@@ -43,11 +77,21 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return new Promise((resolve, reject) => {
+    const queryParams = [user.name, user.email, user.password];
+    let queryStr = `INSERT INTO users (name, email, password)
+    VALUES ($1, $2, $3) RETURNING *;`;
+
+    pool
+      .query(queryStr, queryParams)
+      .then(result => {
+        resolve(result.rows[0]);
+      })
+      .catch(error => reject(error));
+  });
 };
+
+/// Res
 
 /// Reservations
 
